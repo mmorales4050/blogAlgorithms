ee09a0e1ca2cce16af3fa6b628a1e6d2d5443bad
mmorales4050 <mmorales4050@gmail.com>
2019-09-22 23:06:08 -0400

    add reverse int

---
+++ reverseint/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  var reversedInt = parseInt(n.toString().split("").filter((char) => char !== "-").reverse().join(""))
+
+  return reversedInt * Math.sign(n)
+}
+
+module.exports = reverseInt;
