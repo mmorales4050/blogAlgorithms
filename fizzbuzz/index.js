7e68833c25ca13e86b8bc222361aa670303536bc
mmorales4050 <mmorales4050@gmail.com>
2019-10-13 20:44:24 -0400

    add fizzbuzz problem

---
+++ fizzbuzz/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+function fizzBuzz(n) {
+  var counter = 1
+  while(counter <= n){
+    if(counter % 3 === 0 && counter % 5 === 0){
+      console.log("fizzbuzz")
+    }else if(counter % 3 === 0){
+      console.log("fizz")
+    }else if(counter % 5 === 0){
+      console.log("buzz")
+    }else{
+      console.log(counter)
+    }
+    counter = counter + 1
+  }
+}
+
+module.exports = fizzBuzz;
