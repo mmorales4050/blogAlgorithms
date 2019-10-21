7a794716f43f3f5990d9c62aa2acfc19ac71d8cd
mmorales4050 <mmorales4050@gmail.com>
2019-10-20 22:48:53 -0400

    add array chunk

---
+++ chunk/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  var originalSize = size
+  var subArray = []
+  var newArray = []
+  while(array.length > 0){
+    while(size > 0 && array.length > 0){
+      subArray.push(array.shift())
+      size --
+    }
+    newArray.push(subArray)
+    size = originalSize
+    subArray = []
+  }
+  return newArray
+}
+
+module.exports = chunk;
